45918f34cdc566904e7c6a714ae261ff4287c4e5
Dave Welsh <dave@qccareerschool.com>
2026-05-05 16:36:34 -0400

    Added number check

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,12 +1,12 @@
 import { headers } from 'next/headers';
 
 import { SiteLayout } from '@/components/siteLayout';
-import { getRequiredHeader } from '@/lib/getRequiredHeader';
+import { getRequiredNumberHeader } from '@/lib/getRequiredHeader';
 import type { LayoutComponent } from '@/serverComponent';
 
 const MainLayout: LayoutComponent = async ({ children }) => {
   const headersList = await headers();
-  const date = Number(getRequiredHeader(headersList, 'x-current-date'));
+  const date = getRequiredNumberHeader(headersList, 'x-current-date');
 
   return <SiteLayout date={date}>{children}</SiteLayout>;
 };
--- src/lib/getRequiredHeader.ts
+++ src/lib/getRequiredHeader.ts
@@ -1,11 +1,22 @@
 interface HeadersList {
-  get: (n: string) => string | null;
+  get: (name: string) => string | null;
 }
 
 export const getRequiredHeader = (headersList: HeadersList, name: string): string => {
-  const header = headersList.get(name);
-  if (header === null) {
+  const value = headersList.get(name);
+  if (value === null) {
     throw Error(`Header ${name} missing`);
   }
-  return header;
+  return value;
+};
+
+export const getRequiredNumberHeader = (headersList: HeadersList, name: string): number => {
+  const value = getRequiredHeader(headersList, name);
+  const num = Number(value);
+
+  if (Number.isNaN(num)) {
+    throw Error(`Header ${name} is not a valid number`);
+  }
+
+  return num;
 };
