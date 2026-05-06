6f8f761b33d57ec25eb3d067d5d557ecfc2144e1
Dave Welsh <dave@qccareerschool.com>
2026-05-06 15:13:48 -0400

    Adjusted margins and headings

--- src/app/(main)/online-writing-courses/_components/guaranteeSection/index.tsx
+++ src/app/(main)/online-writing-courses/_components/guaranteeSection/index.tsx
@@ -15,21 +15,21 @@ interface Props {
 export const GuaranteeSection: FC<Props> = ({ title, doubleGuarantee, courseCodes, additionalText, className }) => (
   <section className={className}>
     <div className="container">
-      <h2 className="h1 mb-4 text-center">{title} Course Guarantee</h2>
-      <div className="row justify-content-center align-items-center">
-        <div className="col-4 d-none d-lg-block">
-          <Image src={Guarantee} alt="21-day money back guarantee" className="img-fluid" />
+      <div className="row justify-content-center g-4">
+        <div className="col-12 col-lg-3 text-center">
+          <Image src={Guarantee} alt="21-day money back guarantee" className="img-fluid" style={{ width: 240 }} />
         </div>
-        <div className="col-12 col-md-10 col-lg-8">
+        <div className="col-12 col-lg-9">
+          <h2 className="mb-3">{title} Course Guarantee</h2>
           <p>You get a full money-back guarantee for all of our writing programs. You can take the time to review the course materials in your home for 21 days. If for any reason you decide the course is not right for you, simply send everything back to the School and we'll issue you a full refund—no questions asked.</p>
           {doubleGuarantee && (
             <>
-              <h3 className="h5">Double Guarantee</h3>
-              <p>The {title} course is also covered by our Double Guarantee. In addition to the no-risk 21-day trial, we guarantee your success. If by the time you've completed the program you have not earned the equivalent of your course fees through your published work, we'll issue you a full refund. It's as simple as that. Our course is designed for your success and we back up that promise to you with an iron-clad guarantee that's been in place for over twenty years.</p>
+              <h3 className="h4 mt-3">Double Guarantee</h3>
+              <p>The {title} course is also covered by our Double Guarantee. In addition to the no-risk 21-day trial, we guarantee your success. If by the time you've completed the program you have not earned the equivalent of your course fees through your published work, we'll issue you a full refund. It's as simple as that. Our course is designed for your success and we back up that promise to you with an iron-clad guarantee that's been in place for over forty years.</p>
             </>
           )}
           {additionalText && <p>{additionalText}</p>}
-          <a href={`https://enroll.winghill.com?${courseCodes.map(c => `c=${encodeURIComponent(c)}`).join('&')}`} className="btn btn-primary">Enroll Now</a>
+          <a href={`https://enroll.winghill.com?${courseCodes.map(c => `c=${encodeURIComponent(c)}`).join('&')}`} className="btn btn-primary btn-lg">Enroll Now</a>
         </div>
       </div>
     </div>
--- src/app/(main)/online-writing-courses/novel-writing/page.tsx
+++ src/app/(main)/online-writing-courses/novel-writing/page.tsx
@@ -28,12 +28,12 @@ const NovelWritingPage: PageComponent = async ({ searchParams }) => {
           <Image src={CourseBanner} alt="" style={{ maxWidth: '100%', height: 'auto', marginBottom: '0.5rem' }} />
           <p>You might be overwhelmed by the idea of writing an entire novel on your own. We'll let you in on a little secret—it's easier than you think! By writing only a few pages a week, you'll quickly finish your first book. We'll show you how.</p>
           <p>Our course guides break down the process of writing a novel into a few simple steps. The first step involves careful planning and organization. You'll discover that once you've laid out the structure of the book and its characters, writing the novel is actually the easy part! We'll teach you how to create plots, bring in conflict, and craft an effective ending. You'll also learn how to build major and minor characters that are full of life, emotion, and energy. Each lesson of the course is dedicated to helping you harness your creative ideas and mould them into a publishable manuscript.</p>
-          <p>If you've ever read a book and thought &ldquo;I could do better than that!&rdquo; or you've always dreamed of writing a novel, our program will guide you towards success.</p>
+          <p className="mb-0">If you've ever read a book and thought &ldquo;I could do better than that!&rdquo; or you've always dreamed of writing a novel, our program will guide you towards success.</p>
         </div>
       </section>
       <TestimonialWallSection testimonialIds={[]} className="bg-light" />
       <CourseOutlineSection items={outlineItems} />
-      <GuaranteeSection title="Memoir Writing" doubleGuarantee={false} courseCodes={courseCodes} className="bg-light" />
+      <GuaranteeSection title="Novel Writing" doubleGuarantee={false} courseCodes={courseCodes} className="bg-light" />
     </>
   );
 };
