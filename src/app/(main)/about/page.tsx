8b5df4dcaea5d1a3bc067fae78544ee0d965cb2d
Angela Li <angelayxli@gmail.com>
2026-05-06 11:54:39 -0400

    fixed BBB breakpoints and added maxwidth

--- src/app/(main)/about/page.tsx
+++ src/app/(main)/about/page.tsx
@@ -15,7 +15,7 @@ const AboutPage: PageComponent = () => {
     <>
       <section>
         <div className="container">
-          <h1 className="text-center">About Winghill</h1>
+          <h1 className="text-center mb-3">About Winghill</h1>
           <p>Winghill Writing School is a faculty of <Link href="https://www.qccareerschool.com/">QC Career School</Link>. Since 1984, QC Career School has been committed to offering the highest quality distance education experience available. Our writing courses are as old as our school, and we've enjoyed watching over two decades of writing students grow and succeed. We've expanded to offer courses in many creative pursuits such as event and wedding planning, interior decorating, makeup artistry, and fashion styling.</p>
           <p>Winghill Writing School offers a completely integrated learning experience, and the best part is that by learning online you'll be able to pursue your dreams from anywhere, anytime. With Winghill, you'll develop your craft step by step through careful research, planning, writing, and editing. Your <Link href="/your-tutors">tutor</Link> is a published author, and will offer their expert advice to help you reach your writing goals.</p>
           <p>Just as we believe in offering high quality course materials, we're also committed to offering our students the student support experience they need and deserve. That's why you'll not only submit your assignments to a professional writer, but you'll also have unlimited access to our team of highly trained student support specialists by phone, email, live chat, and social media. You'll study from home but you'll never be alone. We're here, and available, to help you whenever you need it.</p>
@@ -24,13 +24,12 @@ const AboutPage: PageComponent = () => {
 
       <section className="bg-light">
         <div className="container">
-          <h2 className="h1 text-center">BBB Accreditation</h2>
+          <h2 className="h1 text-center mb-3">BBB Accreditation</h2>
           <div className="row justify-content-center align-items-center">
-            <div className="col-2">
-              <Image src={BBB} alt="BBB Start With Trust badge" />
+            <div className="col-12 col-lg-2 text-center">
+              <Image src={BBB} alt="BBB Start With Trust badge" className="img-fluid" />
             </div>
-
-            <div className="col-12 col-md-5 text-start">
+            <div className="col-12 col-lg-6 text-start">
               <p>We know that some people might think twice before enrolling in a distance education course. However, you don't need to worry with QC Career School. We have the highest rating of consumer satisfaction possible with the <Link href="https://www.bbb.org/ca/on/ottawa/profile/correspondence-schools/qc-career-school-0117-4175#sealclick">Better Business Bureau (A+)</Link>. In addition, thousands of satisfied students have taken our courses. Many of them are now the proud owners of their own successful business. We sincerely hope that you'll be our next student to follow in their footsteps!</p>
             </div>
           </div>
@@ -38,12 +37,12 @@ const AboutPage: PageComponent = () => {
       </section>
       <section>
         <div className="container text-center">
-          <h2 className="h1 text-center">Our Core Values</h2>
+          <h2 className="h1 text-center mb-3">Our Core Values</h2>
           <p>Our mission is to provide you with a rich and personalized learning experience, all from the comfort of your home.</p>
           <div className="row justify-content-center g-5">
             {coreValues.map(c => (
               <div key={c.title} className="col-10 col-sm-8 col-md-6 col-lg-4">
-                <Image src={c.icon} alt="" className="img-fluid" />
+                <Image src={c.icon} alt="" className="img-fluid" style={{ maxWidth: '160px' }} />
                 <h5>{c.title}</h5>
                 {c.text}
               </div>
@@ -53,7 +52,7 @@ const AboutPage: PageComponent = () => {
       </section>
       <section className="bg-light">
         <div className="container">
-          <h2 className="h1 text-center">Our Guarantee</h2>
+          <h2 className="h1 text-center mb-3">Our Guarantee</h2>
           <div className="row justify-content-center align-items-center">
             <div className="col-12 col-md-4">
               <Image src={Guarantee} alt="money back 100% guarantee" className="img-fluid" />
