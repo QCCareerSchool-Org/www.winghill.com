443154bf493e23aba300ed1d05f194ccbba679dc
Angela Li <angelayxli@gmail.com>
2026-05-06 11:14:20 -0400

    accounted for breakpoints

--- src/app/(main)/about/page.tsx
+++ src/app/(main)/about/page.tsx
@@ -26,11 +26,12 @@ const AboutPage: PageComponent = () => {
         <div className="container">
           <h2 className="h1 text-center">BBB Accreditation</h2>
           <div className="row justify-content-center align-items-center">
-            <div className="col-5 text-start">
-              <p>We know that some people might think twice before enrolling in a distance education course. However, you don't need to worry with QC Career School. We have the highest rating of consumer satisfaction possible with the <Link href="https://www.bbb.org/ca/on/ottawa/profile/correspondence-schools/qc-career-school-0117-4175#sealclick">Better Business Bureau (A+)</Link>. In addition, thousands of satisfied students have taken our courses. Many of them are now the proud owners of their own successful business. We sincerely hope that you'll be our next student to follow in their footsteps!</p>
-            </div>
             <div className="col-2">
               <Image src={BBB} alt="BBB Start With Trust badge" />
+            </div>
+
+            <div className="col-12 col-md-5 text-start">
+              <p>We know that some people might think twice before enrolling in a distance education course. However, you don't need to worry with QC Career School. We have the highest rating of consumer satisfaction possible with the <Link href="https://www.bbb.org/ca/on/ottawa/profile/correspondence-schools/qc-career-school-0117-4175#sealclick">Better Business Bureau (A+)</Link>. In addition, thousands of satisfied students have taken our courses. Many of them are now the proud owners of their own successful business. We sincerely hope that you'll be our next student to follow in their footsteps!</p>
             </div>
           </div>
         </div>
@@ -54,10 +55,10 @@ const AboutPage: PageComponent = () => {
         <div className="container">
           <h2 className="h1 text-center">Our Guarantee</h2>
           <div className="row justify-content-center align-items-center">
-            <div className="col-4">
+            <div className="col-12 col-md-4">
               <Image src={Guarantee} alt="money back 100% guarantee" className="img-fluid" />
             </div>
-            <div className="col-8">
+            <div className="col-12 col-md-8">
               <p>We have absolute faith in our courses. As a result, we offer our students a full money-back guarantee. From the moment you receive your first package, you have 21 days to go over your course materials and assignments. If for any reason you decide that a career in writing is not for you, all you need to do is return your course materials in their original condition for an immediate refund. All we ask of you is to call the School to receive instructions on how to ship the package to us.</p>
               <p>Please note that the Movie Magic Screenwriter software program (value $170 US) can not be refunded.</p>
               <h5>Double Guarantee</h5>
