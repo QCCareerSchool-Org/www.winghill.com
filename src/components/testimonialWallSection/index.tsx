1d63973cfcc5141334e83c67ed2ab806b9a3f9a2
Daphne Huang <daphnehlj25@gmail.com>
2026-05-07 12:07:58 -0700

    remove extra text from testimony wall

--- src/components/testimonialWallSection/index.tsx
+++ src/components/testimonialWallSection/index.tsx
@@ -23,7 +23,6 @@ export const TestimonialWallSection: FC<Props> = ({ h, courseCodes, testimonialI
         <div className="row justify-content-center">
           <div className="col-12 col-lg-9 col-xl-8 col-xxl-7 text-lg-center">
             <h2 className={`mb-4 ${headerClass(h)}`}>Hear From Students and Grads</h2>
-            <p className="lead mb-5">We've helped 45,000+ students and graduates start their own successful design businesses!</p>
           </div>
         </div>
       )}
