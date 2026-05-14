3bf34fa4fdfb028822a2b21b6a4bce7d5be431db
Dave Welsh <dave@qccareerschool.com>
2026-05-14 11:52:17 -0400

    Adjusted spacing

--- src/app/(main)/learning-online/stepSection.tsx
+++ src/app/(main)/learning-online/stepSection.tsx
@@ -1,17 +1,18 @@
 import type { StaticImageData } from 'next/image';
 import Image from 'next/image';
-import type { FC, PropsWithChildren } from 'react';
+import type { CSSProperties, FC, PropsWithChildren } from 'react';
 
 interface Props {
   heading: string;
   src: StaticImageData;
+  width?: CSSProperties['width'];
   className?: string;
 }
 
-export const StepSection: FC<PropsWithChildren<Props>> = ({ heading, src, className, children }) => (
+export const StepSection: FC<PropsWithChildren<Props>> = ({ heading, src, width = 200, className, children }) => (
   <section className={className}>
     <div className="container text-center">
-      <Image src={src} alt="" className="img-fluid" />
+      <Image src={src} alt="" className="img-fluid mb-4" style={{ width }} />
       <h1 className="h2 mb-4">{heading}</h1>
       {children}
     </div>
